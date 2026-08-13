2fad7e1571f35c39831d761a7359480c7e3815e7
wukongdaily <2666180@gmail.com>
2026-08-13 10:53:41 +0800

    feat: 25.12 add mosdns

--- shell/apk-custom-packages.sh
+++ shell/apk-custom-packages.sh
@@ -30,6 +30,8 @@
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-daed-zh-cn"
 # 任务设置 by sirpdboy
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-taskplan luci-i18n-taskplan-zh-cn"
+# MosDNS
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-mosdns luci-i18n-mosdns-zh-cn"
 
 # 仓库内代理相关apk
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-nikki-zh-cn"
